2b15f97f76f61916f05912e4de66383c9d8c603a
hang-shy <946450178@qq.com>
2020-05-07 17:26:29 +0800

    first commit

---
+++ src/test/java/com/dai/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dai.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
